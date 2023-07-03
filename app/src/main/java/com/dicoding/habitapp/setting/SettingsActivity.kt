96d905414adb25f82659c9d283940fcac12993a3
Damantine <firmansyah.mrf@gmail.com>
2023-07-03 21:03:26 +0700

    AWKOAWKOAW KELAR

---
+++ app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -0,0 +1,45 @@
+package com.dicoding.habitapp.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceFragmentCompat
+import com.dicoding.habitapp.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            //TODO 11 : Update theme based on value in ListPreference
+            val themePreference = findPreference<androidx.preference.ListPreference>(getString(R.string.pref_key_dark))
+            themePreference?.setOnPreferenceChangeListener { _, newValue ->
+                when(newValue){
+                    getString(R.string.dark_mode_auto) -> updateTheme(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    getString(R.string.pref_dark_on) -> updateTheme(AppCompatDelegate.MODE_NIGHT_YES)
+                    getString(R.string.pref_dark_off) -> updateTheme(AppCompatDelegate.MODE_NIGHT_NO)
+                    else -> false
+                }
+            }
+        }
+
+        private fun updateTheme(mode: Int): Boolean {
+            AppCompatDelegate.setDefaultNightMode(mode)
+            requireActivity().recreate()
+            return true
+        }
+    }
+}
